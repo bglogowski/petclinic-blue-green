48142e9fabcf0396115b4aeae7e068240d6b464a
Bryan Glogowski <bglogowski@users.noreply.github.com>
2026-02-06 10:48:02 -0800

    Update bootstrap.tf

--- GREEN/bootstrap.tf
+++ GREEN/bootstrap.tf
@@ -1,12 +1,3 @@
 variable "infrastructure_version" {
   default = "1"
 }
-
-terraform {
-  backend "s3" {
-    bucket = "cse-41381-terraform-bucket-green"
-    region = "us-west-2"
-    key    = "terraform.tfstate"
-  }
-}
-
