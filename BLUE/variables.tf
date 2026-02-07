bc7ad6196a7032d46948384a8db4f73d9e673d65
Bryan Glogowski <bglogowski@users.noreply.github.com>
2026-02-07 12:20:56 -0800

    Update variables.tf

--- BLUE/variables.tf
+++ BLUE/variables.tf
@@ -64,7 +64,7 @@ variable "test-name" {
 variable "amis" {
   description = "AMIs by region"
   default = {
-    us-west-2 = "ami-0e19259be9560bb68" # ubuntu 
+    us-west-2 = "ami-047c73e3e019a2567" # ubuntu 
   }
 }
 
