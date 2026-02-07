a5a2852d23263b79fa65f287e91f4848dcaa9f4e
Bryan Glogowski <bglogowski@users.noreply.github.com>
2026-02-07 14:28:10 -0800

    Update variables.tf

--- GREEN/variables.tf
+++ GREEN/variables.tf
@@ -54,7 +54,7 @@ variable "build_id" {
 variable "amis" {
   description = "AMIs by region"
   default = {
-    us-west-2 = "ami-047c73e3e019a2567" # ubuntu 
+    us-west-2 = "ami-00d0ecca5d4f95941" # ubuntu 
   }
 }
 
